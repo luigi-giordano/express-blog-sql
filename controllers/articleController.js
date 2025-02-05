36e2c1ec3f6d2922e1b5b5f0bea87b100953a71a
luigi-giordano <luigi.giordano1992@gmail.com>
2025-02-05 17:15:23 +0100

    milestone 2

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,15 +1,17 @@
 //import connection db
+const { log } = require('node:console');
 const connection = require('../data/db')
 
 
-
 const index = (req, res) => {
-  const sql = 'SELECT * FROM articles';
+  const sql = 'SELECT * FROM posts';
 
   //query al db
   connection.query(sql, (err, results) => {
+    console.log(err);
     if (err) return res.status(500).json({ error: 'Query al database fallita' })
-    res.json(results)
+
+    res.json(results);
   })
 
 }
