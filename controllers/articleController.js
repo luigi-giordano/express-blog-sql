61e2aba5568eb4a6823f15d8271013c2f3850372
luigi-giordano <luigi.giordano1992@gmail.com>
2025-02-05 15:40:36 +0100

    router + controller

---
+++ controllers/articleController.js
@@ -0,0 +1,40 @@
+const index = (req, res) => {
+  res.send('Server Articoli')
+}
+
+const show = (req, res) => {
+
+  const id = req.params.id
+  res.send(`Dettaglio Articolo ${id}`)
+}
+
+const store = (req, res) => {
+  res.send('Creazione Articolo')
+}
+
+const update = (req, res) => {
+
+  const id = req.params.id
+  res.send(`Modifica Articolo ${id}`)
+}
+
+const modify = (req, res) => {
+
+  const id = req.params.id
+  res.send(`Modifica parziale Articolo ${id}`)
+}
+
+const destroy = (req, res) => {
+
+  const id = req.params.id
+  res.send(`Eliminazione Articolo ${id}`)
+}
+
+module.exports = {
+  index,
+  show,
+  store,
+  update,
+  modify,
+  destroy
+}
