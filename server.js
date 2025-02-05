cd13ec4179f36811a6c30e861597f71b15a0f714
luigi-giordano <luigi.giordano1992@gmail.com>
2025-02-05 15:05:10 +0100

    middleware vari

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-const express = require('express')
+const { log } = require('console');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+//middleware assets static
+app.use(express.static('public'));
+
+//middleware parsing body
+app.use(express.json());
+
+//rotta 
+app.get('/', (req, res) => {
+  res.send('Server del Blog')
+})
+
+//ascolto porta
+app.listen(port, () => {
+  console.log('Sono in ascolto alla porta 3000');
+
+})
